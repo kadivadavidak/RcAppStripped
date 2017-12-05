3995bd7c590261a1ab5dbccd9f7c3d96373dec9f
David Kirschman <kadivadavidak@gmail.com>
2017-12-05 12:09:33 -0700

    Initialize repo

---
+++ README.md
@@ -0,0 +1,19 @@
+RingCentral Data Integration App
+================================
+
+| [/api-docs/][1] | [/login/][2] | [/support/][3] |
+| --------------- | ------------ | -------------- |
+
+## Functionality
+
+1. Get the requested data.
+2. Insert data into staging table in the TARDIS DB.
+3. Move to next integration if any.
+
+## App location
+
+The app is run from the SQL server at Archive\ImportApps\RingCentralDataIntegration
+
+[1]: https://developer.ringcentral.com/api-docs/latest/index.html
+[2]: https://developer.ringcentral.com/login.html
+[3]: https://developers.ringcentral.com/support.html
---
+++ RingCentralDataIntegration/SmtpHandler.cs
@@ -0,0 +1,30 @@
+﻿using System.Net;
+using System.Net.Mail;
+
+namespace RingCentralDataIntegration
+{
+    class SmtpHandler
+    {
+        internal static void SendMessage(string subject, string body)
+        {
+            var email = new MailMessage("donotreply@springmobile.com", "david.kirschman@springmobile.com") //"reporting@springmobile.com"
+            {
+                Subject = subject,
+                Body = body
+            };
+
+            using (var client = new SmtpClient())
+            {
+                client.Port = 25;
+                client.DeliveryMethod = SmtpDeliveryMethod.Network;
+                client.UseDefaultCredentials = false;
+                client.Host = "";
+                client.EnableSsl = true;
+                client.Credentials = new NetworkCredential("",
+                    "");
+
+                client.Send(email);
+            }
+        }
+    }
+}
